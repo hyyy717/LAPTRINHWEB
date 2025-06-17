40733716aa69abb2efa38241ce16c61d0f659fd7
hyyy717 <biluu174@gmail.com>
2025-06-18 01:58:41 +0700

    Add project files.

---
+++ BanSach/Data/BookStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BanSach.Data;
+
+public class BookStoreContext : DbContext
+{
+    public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+    {
+    }
+
+    public DbSet<BanSach.Models.Book> Books { get; set; }
+}
---
+++ BanSach/Program.cs
@@ -0,0 +1,34 @@
+using BanSach.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<BookStoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
